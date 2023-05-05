bd47a3934d0ade5fccdd5e64fee946d5e9028445
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-05 18:37:27 -0300

    Add files via upload

--- plugins/nv-a.js
+++ plugins/nv-a.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/a.mp3'
 conn.sendPresenceUpdate('recording', m.chat)
-conn.sendMessage(m.chat, { audio: { url: vn }, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `a.mp3` }, { quoted: m })}
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": `👑 ɴᴏᴠᴀʙᴏᴛ-ᴍᴅ 👑`, "body": ``, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen1, "sourceUrl": `https://github.com/elrebelde21/NovaBot-MD`, "showAdAttribution": true}}, seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
 handler.customPrefix = /ª|a|A/
 handler.command = /^(a|ª|A?$)/
 export default handler
